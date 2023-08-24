51e629943920825242500fd5525afcecec71961f
Vamsy Kiran A <a.vamc.it@gmail.com>
2023-08-24 17:39:57 +0530

    HttpClientModule communication with rest-api

--- app05/src/app/txns/txns.component.ts
+++ app05/src/app/txns/txns.component.ts
@@ -12,33 +12,49 @@ import { ConsumerService } from '../services/consumer.service';
 })
 export class TxnsComponent implements OnInit {
 
-  consumer: Consumer | undefined;
+  consumer!: Consumer;
   txns!: Txn[]
+  errMsg!:string;
 
-  constructor(private consumerService: ConsumerService, private txnsServcie: TxnsService, private activatedRoute: ActivatedRoute) {
+  constructor(private consumerService: ConsumerService, private txnsService: TxnsService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     let cid = this.activatedRoute.snapshot.params["cid"];
     if (cid) {
-      this.consumer = this.consumerService.getById(Number(cid));
-      if (this.consumer) {
-        this.txns = this.txnsServcie.getAllByConsumerId(this.consumer.id);
-      }
+      this.consumerService.getById(Number(cid)).subscribe({
+        next: consumer => {
+          this.consumer=consumer;
+          this.loadTxns();
+        },
+        error: err => {console.log(err); this.errMsg="Unable to retive data!"}
+      });
     }
+  }
+
+  loadTxns(){
+    this.txnsService.getAllByConsumerId(this.consumer.id).subscribe({
+      next: txns=> this.txns=txns,
+      error: err => {console.log(err); this.errMsg="Unable to retive data!"}
+    });
   }
 
   remove(txnid:number){
     if(window.confirm("Are you sure of deleting the transaction?")){
-      this.txnsServcie.deleteById(txnid);
-      this.txns = this.txnsServcie.getAllByConsumerId(this.consumer!.id);
+      this.txnsService.deleteById(txnid).subscribe({
+        next: () => this.loadTxns(),
+        error: err => {console.log(err); this.errMsg="Unable to delete data!"}
+      })
     }    
   }
 
   add(txn:Txn){
     txn.cid=this.consumer?.id??0;
-    this.txnsServcie.add(txn);
-    this.txns = this.txnsServcie.getAllByConsumerId(this.consumer!.id);
+
+    this.txnsService.add(txn).subscribe({
+      next: data => this.loadTxns(),
+      error: err => {console.log(err); this.errMsg="Unable to save data!"}
+    })
   }
 
   markEditable(id:number){
@@ -51,7 +67,9 @@ export class TxnsComponent implements OnInit {
   update(txn:Txn){
     txn.cid=this.consumer?.id??0;
     txn.isEditing=undefined;
-    this.txnsServcie.update(txn);
-    this.txns = this.txnsServcie.getAllByConsumerId(this.consumer!.id);
+    this.txnsService.update(txn).subscribe({
+      next: data => this.loadTxns(),
+      error: err => {console.log(err); this.errMsg="Unable to save data!"}
+    })
   }
 }
